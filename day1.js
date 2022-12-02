c031e0419c611868de34d9155e3741e00d4e3cbf
sergioedo <sergio.edo@gmail.com>
2022-12-02 09:58:15 +0100

    day1 - sample test passed

--- day1.js
+++ day1.js
@@ -1,1 +1,17 @@
-export const maxElfCalories = () => {}
+import fs from 'fs'
+
+export const maxElfCalories = (inputFile) => {
+    const calories = fs.readFileSync(inputFile, { encoding: 'UTF-8' }).split('\n')
+    let maxCalories = 0
+    let currElfCalories = 0
+    for (let i = 0; i < calories.length; i++) {
+        const cal = calories[i]
+        if (cal.length > 0) { //add calories to current elf
+            currElfCalories += Number(cal)
+        } else { //next elf
+            if (currElfCalories > maxCalories) maxCalories = currElfCalories
+            currElfCalories = 0
+        }
+    }
+    return maxCalories
+}
