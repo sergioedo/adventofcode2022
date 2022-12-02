a93695c83cd7ee438f1a13912ea4004370f82bb4
sergioedo <sergio.edo@gmail.com>
2022-12-02 10:01:24 +0100

    day1 - fix name tests

--- day1.test.js
+++ day1.test.js
@@ -2,14 +2,14 @@ import { expect, test } from 'vitest'
 import { maxElfCalories } from 'day1'
 import fs from 'fs'
 
-test('Test sample input', () => {
+test('Test maxElfCalories sample input', () => {
     const inputFile = 'day1.input.sample.txt'
     const expectedOutput = 24000
 
     expect(maxElfCalories(inputFile)).toBe(expectedOutput)
 })
 
-test('Test sample input', () => {
+test('Test maxElfCalories input', () => {
     const inputFile = 'day1.input.txt'
     const expectedOutput = 69836
 
