43ae434a3dcbeb0c4e4273e7d2b970169411045f
sergioedo <sergio.edo@gmail.com>
2022-12-02 11:12:41 +0100

    day2 - part2

--- day2/index.js
+++ day2/index.js
@@ -24,15 +24,43 @@ const scoreByResult = {
     }
 }
 
-const scoreByRound = (opponent, me) => {
+const scoreByRound = (opponent, me, scoreByOption, scoreByResult) => {
     return scoreByOption[me] + scoreByResult[me][opponent]
 }
 
-export const totalScore = (inputFile) => {
+export const totalScoreByFunction = (inputFile, scoreByOption, scoreByResult) => {
     const strategy = fs.readFileSync(inputFile, { encoding: 'UTF-8' }).split('\n')
         .map(row => row.split(' '))
-    const score = strategy.map(round => scoreByRound(round[0], round[1]))
+    const score = strategy.map(round => scoreByRound(round[0], round[1], scoreByOption, scoreByResult))
         .reduce((prev, curr) => prev + curr, 0)
     return score
 
 }
+
+export const totalScore = (inputFile) => totalScoreByFunction(inputFile, scoreByOption, scoreByResult)
+
+const scoreByOptionPart2 = {
+    'X': 0,
+    'Y': 3,
+    'Z': 6
+}
+
+const scoreByResultPart2 = {
+    'X': {
+        'A': scoreByOption['Z'],
+        'B': scoreByOption['X'],
+        'C': scoreByOption['Y'],
+    },
+    'Y': {
+        'A': scoreByOption['X'],
+        'B': scoreByOption['Y'],
+        'C': scoreByOption['Z'],
+    },
+    'Z': {
+        'A': scoreByOption['Y'],
+        'B': scoreByOption['Z'],
+        'C': scoreByOption['X'],
+    }
+}
+
+export const totalScoreNewStrategy = (inputFile) => totalScoreByFunction(inputFile, scoreByOptionPart2, scoreByResultPart2)
--- day2/index.test.js
+++ day2/index.test.js
@@ -1,5 +1,5 @@
 import { expect, test } from 'vitest'
-import { totalScore } from 'day2'
+import { totalScore, totalScoreNewStrategy } from 'day2'
 import path from 'path'
 
 test('Test totalScore sample input', () => {
@@ -14,4 +14,18 @@ test('Test totalScore input', () => {
     const expectedOutput = 12156
 
     expect(totalScore(inputFile)).toBe(expectedOutput)
+})
+
+test('Test totalScore with new strategy sample input', () => {
+    const inputFile = path.join('day2', 'input.sample.txt')
+    const expectedOutput = 12
+
+    expect(totalScoreNewStrategy(inputFile)).toBe(expectedOutput)
+})
+
+test('Test totalScore with new strategy sample', () => {
+    const inputFile = path.join('day2', 'input.txt')
+    const expectedOutput = 10835
+
+    expect(totalScoreNewStrategy(inputFile)).toBe(expectedOutput)
 })
