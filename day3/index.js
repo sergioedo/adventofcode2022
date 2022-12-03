ea6ecc7cf2bdd2803a3c0b7baf76336701ef73f7
sergioedo <sergio.edo@gmail.com>
2022-12-03 07:20:26 +0100

    day3 - part2

--- day3/index.js
+++ day3/index.js
@@ -8,6 +8,12 @@ const getSharedItem = (rucksack) => {
     return firstCompartment.split('').find(item => secondCompartment.includes(item))
 }
 
+const getGroupSharedItem = (group) => {
+    const [firstElement, ...restElements] = group
+
+    return firstElement.split('').find(item => restElements.every(element => element.includes(item)))
+}
+
 const getItemPriority = (item) => {
     const charCode = item.charCodeAt(0)
     if (charCode >= 97) return charCode - 97 + 1 // [a-z]
@@ -18,5 +24,17 @@ export const sumPriorities = (inputFile) => {
     const rucksacks = fs.readFileSync(inputFile, { encoding: 'UTF-8' }).split('\n')
     const sharedItems = rucksacks.map(getSharedItem)
 
+    return sharedItems.reduce((prev, curr) => prev + getItemPriority(curr), 0)
+}
+
+export const sumGroupPriorities = (inputFile) => {
+    const rucksacks = fs.readFileSync(inputFile, { encoding: 'UTF-8' }).split('\n')
+    const rucksacksGroups = Object.values(rucksacks.reduce((prev, curr, currIdx) => {
+        const groupIdx = Math.floor(currIdx / 3)
+        if (!prev[groupIdx]) prev[groupIdx] = []
+        prev[groupIdx].push(curr)
+        return prev
+    }, {}))
+    const sharedItems = rucksacksGroups.map(getGroupSharedItem)
     return sharedItems.reduce((prev, curr) => prev + getItemPriority(curr), 0)
 }
--- day3/index.test.js
+++ day3/index.test.js
@@ -1,5 +1,5 @@
 import { expect, test } from 'vitest'
-import { sumPriorities } from 'day3'
+import { sumPriorities, sumGroupPriorities } from 'day3'
 import path from 'path'
 
 test('Test sumPriorities with sample input', () => {
@@ -14,4 +14,18 @@ test('Test sumPriorities with input', () => {
     const expectedOutput = 7811
 
     expect(sumPriorities(inputFile)).toBe(expectedOutput)
+})
+
+test('Test sumGroupPriorities with sample input', () => {
+    const inputFile = path.join('day3', 'input.sample.txt')
+    const expectedOutput = 70
+
+    expect(sumGroupPriorities(inputFile)).toBe(expectedOutput)
+})
+
+test('Test sumGroupPriorities with input', () => {
+    const inputFile = path.join('day3', 'input.txt')
+    const expectedOutput = 2639
+
+    expect(sumGroupPriorities(inputFile)).toBe(expectedOutput)
 })
