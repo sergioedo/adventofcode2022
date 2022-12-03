f7bc3cf917275675bc1a4f0521620c3756e21084
sergioedo <sergio.edo@gmail.com>
2022-12-03 07:35:29 +0100

    day3 - refactor

--- day3/index.js
+++ day3/index.js
@@ -1,14 +1,7 @@
 import fs from 'fs'
 import { isFunction } from 'util'
 
-const getSharedItem = (rucksack) => {
-    const firstCompartment = rucksack.substring(0, rucksack.length / 2)
-    const secondCompartment = rucksack.substring(rucksack.length / 2)
-
-    return firstCompartment.split('').find(item => secondCompartment.includes(item))
-}
-
-const getGroupSharedItem = (group) => {
+const getSharedItem = (group) => {
     const [firstElement, ...restElements] = group
 
     return firstElement.split('').find(item => restElements.every(element => element.includes(item)))
@@ -20,21 +13,33 @@ const getItemPriority = (item) => {
     return charCode - 65 + 27 // [A-Z]
 }
 
-export const sumPriorities = (inputFile) => {
-    const rucksacks = fs.readFileSync(inputFile, { encoding: 'UTF-8' }).split('\n')
-    const sharedItems = rucksacks.map(getSharedItem)
+const getRucksackParts = (input) => {
+    return input.map(rucksack => {
+        const firstCompartment = rucksack.substring(0, rucksack.length / 2)
+        const secondCompartment = rucksack.substring(rucksack.length / 2)
+        return [firstCompartment, secondCompartment]
+    })
+}
 
+const sumSharedItemsPriorities = (inputFile, getGroupedItems) => {
+    const input = fs.readFileSync(inputFile, { encoding: 'UTF-8' }).split('\n')
+    const sharedItems = getGroupedItems(input).map(getSharedItem)
     return sharedItems.reduce((prev, curr) => prev + getItemPriority(curr), 0)
 }
 
-export const sumGroupPriorities = (inputFile) => {
-    const rucksacks = fs.readFileSync(inputFile, { encoding: 'UTF-8' }).split('\n')
-    const rucksacksGroups = Object.values(rucksacks.reduce((prev, curr, currIdx) => {
+export const sumPriorities = (inputFile) => {
+    return sumSharedItemsPriorities(inputFile, getRucksackParts)
+}
+
+const getRucksackGroups = (input) => {
+    return Object.values(input.reduce((prev, curr, currIdx) => {
         const groupIdx = Math.floor(currIdx / 3)
         if (!prev[groupIdx]) prev[groupIdx] = []
         prev[groupIdx].push(curr)
         return prev
     }, {}))
-    const sharedItems = rucksacksGroups.map(getGroupSharedItem)
-    return sharedItems.reduce((prev, curr) => prev + getItemPriority(curr), 0)
+}
+
+export const sumGroupPriorities = (inputFile) => {
+    return sumSharedItemsPriorities(inputFile, getRucksackGroups)
 }
