6d3d0f996ff7813386da0513085e9d35f0f97db2
sergioedo <sergio.edo@gmail.com>
2022-12-09 00:41:52 +0100

    day8 - part2

--- day8/index.js
+++ day8/index.js
@@ -1,17 +1,17 @@
 import fs from 'fs'
 
 const isVisible = (grid, gridSize, rowT, colT) => {
-    //trees on row
-    const leftTrees = [], rightTrees = []
-    for (let row = 0; row < gridSize; row++) {
-        if (row < rowT) leftTrees.push(grid[row][colT])
-        if (row > rowT) rightTrees.push(grid[row][colT])
-    }
     //trees on col
     const upTrees = [], downTrees = []
+    for (let row = 0; row < gridSize; row++) {
+        if (row < rowT) upTrees.push(grid[row][colT])
+        if (row > rowT) downTrees.push(grid[row][colT])
+    }
+    //trees on row
+    const leftTrees = [], rightTrees = []
     for (let col = 0; col < gridSize; col++) {
-        if (col < colT) upTrees.push(grid[rowT][col])
-        if (col > colT) downTrees.push(grid[rowT][col])
+        if (col < colT) leftTrees.push(grid[rowT][col])
+        if (col > colT) rightTrees.push(grid[rowT][col])
     }
     const treeHeight = grid[rowT][colT]
     return leftTrees.every(height => height < treeHeight) ||
@@ -30,4 +30,49 @@ export const countVisibleTree = (inputFile) => {
         }
     }
     return (gridSize * gridSize) - countNoVisible
+}
+
+const treeScore = (grid, gridSize, rowT, colT) => {
+    const treeHeight = grid[rowT][colT]
+    //trees view on top
+    const upTrees = []
+    const downTrees = []
+    const leftTrees = []
+    const rightTrees = []
+    let lastHeight = treeHeight - 1
+    for (let row = rowT - 1; row >= 0 && lastHeight < treeHeight; row--) {
+        lastHeight = grid[row][colT]
+        upTrees.push(lastHeight)
+    }
+    //trees view on bottom
+    lastHeight = treeHeight - 1
+    for (let row = rowT + 1; row < gridSize && lastHeight < treeHeight; row++) {
+        lastHeight = grid[row][colT]
+        downTrees.push(lastHeight)
+    }
+    //trees view on left
+    lastHeight = treeHeight - 1
+    for (let col = colT - 1; col >= 0 && lastHeight < treeHeight; col--) {
+        lastHeight = grid[rowT][col]
+        leftTrees.push(lastHeight)
+    }
+    //trees view on right
+    lastHeight = treeHeight - 1
+    for (let col = colT + 1; col < gridSize && lastHeight < treeHeight; col++) {
+        lastHeight = grid[rowT][col]
+        rightTrees.push(lastHeight)
+    }
+
+    return leftTrees.length * rightTrees.length * upTrees.length * downTrees.length
+}
+
+export const highestTreeScore = (inputFile) => {
+    const grid = fs.readFileSync(inputFile, { encoding: 'UTF-8' }).split('\n').map(row => row.split(''))
+    const gridSize = grid.length  // grid: rows == cols
+    const treeScores = grid.map((row, rowIdx) => {
+        return row.map((col, colIdx) => {
+            return treeScore(grid, gridSize, rowIdx, colIdx)
+        })
+    })
+    return treeScores.flat().sort((a, b) => b - a)[0]
 }
--- day8/index.test.js
+++ day8/index.test.js
@@ -1,5 +1,5 @@
 import { expect, test } from 'vitest'
-import { countVisibleTree } from 'day8'
+import { countVisibleTree, highestTreeScore } from 'day8'
 import fs from 'fs'
 import path from 'path'
 
@@ -13,4 +13,16 @@ test('Test countVisibleTrees with input', () => {
     const inputFile = path.join('day8', 'input.txt')
 
     expect(countVisibleTree(inputFile)).toBe(1851)
+})
+
+test('Test highestTreeScore with input sample', () => {
+    const inputFile = path.join('day8', 'input.sample.txt')
+
+    expect(highestTreeScore(inputFile)).toBe(8)
+})
+
+test('Test highestTreeScore with input', () => {
+    const inputFile = path.join('day8', 'input.txt')
+
+    expect(highestTreeScore(inputFile)).toBe(574080)
 })
