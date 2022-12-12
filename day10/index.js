bc76d44e2a27db86678bc5652ca8a51d7a07b8fe
sergioedo <sergio.edo@gmail.com>
2022-12-12 18:53:54 +0100

    day10 - part2

--- day10/index.js
+++ day10/index.js
@@ -1,6 +1,6 @@
 import fs from 'fs'
 
-export const sumSignalsStrength = (inputFile, sumCycles) => {
+const valueCycles = (inputFile) => {
     const instructions = fs.readFileSync(inputFile, { encoding: 'UTF-8' }).split('\n')
     let x = 1
     const cycles = []
@@ -15,9 +15,26 @@ export const sumSignalsStrength = (inputFile, sumCycles) => {
             x += addValue
         }
     })
+    return cycles
+}
 
+export const sumSignalsStrength = (inputFile, sumCycles) => {
+    const cycles = valueCycles(inputFile)
     return sumCycles.reduce((prev, curr) => {
         const currentStrength = curr * cycles[curr - 1]
         return prev + currentStrength
     }, 0)
+}
+
+export const drawCRT = (inputFile) => {
+    const cycles = valueCycles(inputFile)
+    const crt = new Array(6)  //6 lines
+    for (let cycle = 1; cycle <= cycles.length; cycle++) {
+        const row = Math.floor((cycle - 1) / 40)
+        const col = Math.floor((cycle - 1) % 40)
+        const x = cycles[cycle - 1]
+        const pixel = Math.abs(x - col) > 1 ? '.' : '#'
+        crt[row] = (crt[row] || '') + pixel
+    }
+    return crt
 }
--- day10/index.test.js
+++ day10/index.test.js
@@ -1,5 +1,5 @@
 import { expect, test } from 'vitest'
-import { sumSignalsStrength } from 'day10'
+import { sumSignalsStrength, drawCRT } from 'day10'
 import path from 'path'
 
 test('Test sumSignalsStrength with input sample', () => {
@@ -12,4 +12,32 @@ test('Test sumSignalsStrength with input', () => {
     const inputFile = path.join('day10', 'input.txt')
 
     expect(sumSignalsStrength(inputFile, [20, 60, 100, 140, 180, 220])).toBe(11780)
+})
+
+test('Test drawCRT with input sample', () => {
+    const inputFile = path.join('day10', 'input.sample.txt')
+    const outputCRT = [
+        '##..##..##..##..##..##..##..##..##..##..',
+        '###...###...###...###...###...###...###.',
+        '####....####....####....####....####....',
+        '#####.....#####.....#####.....#####.....',
+        '######......######......######......####',
+        '#######.......#######.......#######.....'
+    ]
+
+    expect(drawCRT(inputFile)).toStrictEqual(outputCRT)
+})
+
+test('Test drawCRT with input', () => {
+    const inputFile = path.join('day10', 'input.txt')
+    const outputCRT = [
+        "###..####.#..#.#....###...##..#..#..##..",
+        "#..#....#.#..#.#....#..#.#..#.#..#.#..#.",
+        "#..#...#..#..#.#....###..#..#.#..#.#..#.",
+        "###...#...#..#.#....#..#.####.#..#.####.",
+        "#....#....#..#.#....#..#.#..#.#..#.#..#.",
+        "#....####..##..####.###..#..#..##..#..#.",
+    ]
+
+    expect(drawCRT(inputFile)).toStrictEqual(outputCRT)
 })
